ef15e02988ea1027fcfb0e6c6d42624504ed4433
Jakob Kogler <jakob.kogler@gmail.com>
2014-09-28 13:39:46 +0200

    ADD abstract class for generating tables

---
+++ src/group.cpp
@@ -0,0 +1,16 @@
+#include "group.h"
+
+void Group::buildTransitionTable()
+{
+	transition_table = vector<vector<long> >(state_count, vector<long>(6));
+	vector<int> arr(piece_count);
+
+	for (int index = 0; index < state_count; index++)
+	{
+		for (int move = 0; move < 6; move++)
+		{
+			index_to_array(index, arr);
+			transition_table[index][move] = array_to_index(arr);
+		}
+	}
+}
---
+++ src/group.h
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Group
+{
+protected: 
+	int state_count;
+	int piece_count;
+	vector<vector<long> > transition_table;
+	vector<int> prune_table;
+	
+public:
+	vector<vector<long> > getTransitionTable(){ return transition_table; }
+	vector<int> getPruneTable(){ return prune_table; }
+	void buildTransitionTable();
+protected:
+	Group(int state_count, int piece_count) : state_count(state_count), piece_count(piece_count) {}
+	virtual void index_to_array(int index, vector<int> & arr) = 0;
+	virtual void apply_move(vector<int> & arr, int move) = 0;
+	virtual int array_to_index(vector<int> const& arr) = 0;
+};
