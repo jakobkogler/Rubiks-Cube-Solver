a0d2c27f6b98709a50cac67fa6ab95bfcf51a145
Jakob Kogler <jakob.kogler@gmail.com>
2014-09-30 21:04:06 +0200

    CHG update test

--- src/test.cpp
+++ src/test.cpp
@@ -7,9 +7,9 @@ int main()
 {
 	OptimalSolver os;
 
-	// two switched adjacent corners
-	os.IDA(vector<int>{ 1, 0, 2, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 });
+	// A-perm
+	os.IDA(vector<int>{ 1, 2, 0, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 1, 2, 3, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
 
-	// two switched opposite corners
-	os.IDA(vector<int>{ 2, 1, 0, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 });
+	// E-perm
+	os.IDA(vector<int>{ 1, 0, 3, 2, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 1, 2, 3, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
 }
