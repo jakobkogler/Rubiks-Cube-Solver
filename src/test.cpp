962e7ca0c52fe91867d13e3e0dadbbbb14b90dfa
Jakob Kogler <jakob.kogler@gmail.com>
2014-09-30 21:26:21 +0200

    ADD more tests

--- src/test.cpp
+++ src/test.cpp
@@ -8,8 +8,17 @@ int main()
 	OptimalSolver os;
 
 	// A-perm
-	os.IDA(vector<int>{ 1, 2, 0, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 1, 2, 3, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
+	os.IDA(vector<int>{ 2, 0, 1, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 1, 2, 3, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
 
 	// E-perm
 	os.IDA(vector<int>{ 1, 0, 3, 2, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 1, 2, 3, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
+
+	// T-perm
+	os.IDA(vector<int>{ 0, 2, 1, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 3, 2, 1, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
+
+	// Y-perm
+	os.IDA(vector<int>{ 2, 1, 0, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 3, 1, 2, 0, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
+
+	// Z-perm
+	os.IDA(vector<int>{ 0, 1, 2, 3, 4, 5, 6, 7 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 0, 0, 0, 0, 0, 0, 0, 0 }, vector<int>{ 1, 0, 3, 2, 4, 5 }, vector<int>{ 6, 7, 8, 9, 10, 11, 12 });
 }
