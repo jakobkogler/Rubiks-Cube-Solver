bc0a73ad3f2f2ceadc15d53ff179dc6438ad3b00
Jakob Kogler <jakob.kogler@gmail.com>
2018-06-17 21:01:42 +0200

    add virtual destructor

--- src/pruning/prune.h
+++ src/pruning/prune.h
@@ -12,6 +12,7 @@ protected:
 public:
     virtual int pruning_number(Cube &cube) = 0;
     virtual void showPruneInfos(std::ostream& os) const;
+    virtual ~Prune() {}
 protected:
     void buildPruneTable(std::vector<std::vector<long long>> &transition_table, int state_count, int start_value = 0);
     int pruneTreeSearch(int state, std::vector<char> & table, char depth_left, char depth, int lastMove, std::vector<std::vector<long long>> &transition_table);
