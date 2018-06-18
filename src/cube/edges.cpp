21c50fb4e401e62e02b4aef3c4598310a393b9c1
Jakob Kogler <jakob.kogler@gmail.com>
2018-06-18 14:49:45 +0200

    Clean up edges.cpp

--- src/cube/edges.cpp
+++ src/cube/edges.cpp
@@ -1,155 +1,48 @@
 #include "edges.h"
+#include <numeric>
 
-Edges::Edges() {
-    edges_perm = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-    edges_orient = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+Table12 generatePermTable(int a, int b, int c, int d) {
+    Table12 t;
+    std::iota(t.begin(), t.end(), 0);
+    t[a] = b;
+    t[b] = c;
+    t[c] = d;
+    t[d] = a;
+    return t;
+}
+
+std::array<int, 12> generateOrientTable(int a, int b, int c, int d) {
+    Table12 t{ 0 };
+    t[a] = t[b] = t[c] = t[d] = 1;
+    return t;
+}
+
+Edges::Edges(int cnt) : cnt(cnt) {
+    edges_perm.assign(cnt, 0);
+    std::iota(edges_perm.begin(), edges_perm.end(), 0);
+    edges_orient.assign(cnt, 0);
+
+    perm[0] = generatePermTable(0, 1, 2, 3);
+    perm[1] = generatePermTable(8, 11, 10, 9);
+    perm[2] = generatePermTable(1, 5, 9, 6);
+    perm[3] = generatePermTable(3, 7, 11, 4);
+    perm[4] = generatePermTable(2, 6, 10, 7);
+    perm[5] = generatePermTable(0, 4, 8, 5);
+    orient[4] = generateOrientTable(2, 6, 10, 7);
+    orient[5] = generateOrientTable(0, 4, 8, 5);
 }
 
 void Edges::apply_move(int move) {
-    if (move == 0)
-    {
-        //move U
-        for (int i = 0; i < 12; i++)
-        {
-            if (edges_perm[i] == 0)
-            {
-                edges_perm[i] = 1;
-            }
-            else if (edges_perm[i] == 1)
-            {
-                edges_perm[i] = 2;
-            }
-            else if (edges_perm[i] == 2)
-            {
-                edges_perm[i] = 3;
-            }
-            else if (edges_perm[i] == 3)
-            {
-                edges_perm[i] = 0;
-            }
+    auto const& p = perm[move];
+    auto const& o = orient[move];
+    if (move < 4) {
+        for (int i = 0; i < cnt; i++) {
+            edges_perm[i] = p[edges_perm[i]];
         }
-    }
-    else if (move == 1)
-    {
-        //move D
-        for (int i = 0; i < 12; i++)
-        {
-            if (edges_perm[i] == 8)
-            {
-                edges_perm[i] = 11;
-            }
-            else if (edges_perm[i] == 11)
-            {
-                edges_perm[i] = 10;
-            }
-            else if (edges_perm[i] == 10)
-            {
-                edges_perm[i] = 9;
-            }
-            else if (edges_perm[i] == 9)
-            {
-                edges_perm[i] = 8;
-            }
-        }
-    }
-    else if (move == 2)
-    {
-        //move R
-        for (int i = 0; i < 12; i++)
-        {
-            if (edges_perm[i] == 1)
-            {
-                edges_perm[i] = 5;
-            }
-            else if (edges_perm[i] == 5)
-            {
-                edges_perm[i] = 9;
-            }
-            else if (edges_perm[i] == 9)
-            {
-                edges_perm[i] = 6;
-            }
-            else if (edges_perm[i] == 6)
-            {
-                edges_perm[i] = 1;
-            }
-        }
-    }
-    else if (move == 3)
-    {
-        //move L
-        for (int i = 0; i < 12; i++)
-        {
-            if (edges_perm[i] == 3)
-            {
-                edges_perm[i] = 7;
-            }
-            else if (edges_perm[i] == 7)
-            {
-                edges_perm[i] = 11;
-            }
-            else if (edges_perm[i] == 11)
-            {
-                edges_perm[i] = 4;
-            }
-            else if (edges_perm[i] == 4)
-            {
-                edges_perm[i] = 3;
-            }
-        }
-    }
-    else if (move == 4)
-    {
-        //move F
-        for (int i = 0; i < 12; i++)
-        {
-            if (edges_perm[i] == 2)
-            {
-                edges_perm[i] = 6;
-                edges_orient[i] ^= 1;
-            }
-            else if (edges_perm[i] == 6)
-            {
-                edges_perm[i] = 10;
-                edges_orient[i] ^= 1;
-            }
-            else if (edges_perm[i] == 10)
-            {
-                edges_perm[i] = 7;
-                edges_orient[i] ^= 1;
-            }
-            else if (edges_perm[i] == 7)
-            {
-                edges_perm[i] = 2;
-                edges_orient[i] ^= 1;
-            }
-        }
-    }
-    else if (move == 5)
-    {
-        //move B
-        for (int i = 0; i < 12; i++)
-        {
-            if (edges_perm[i] == 0)
-            {
-                edges_perm[i] = 4;
-                edges_orient[i] ^= 1;
-            }
-            else if (edges_perm[i] == 4)
-            {
-                edges_perm[i] = 8;
-                edges_orient[i] ^= 1;
-            }
-            else if (edges_perm[i] == 8)
-            {
-                edges_perm[i] = 5;
-                edges_orient[i] ^= 1;
-            }
-            else if (edges_perm[i] == 5)
-            {
-                edges_perm[i] = 0;
-                edges_orient[i] ^= 1;
-            }
+    } else {
+        for (int i = 0; i < cnt; i++) {
+            edges_orient[i] ^= o[edges_perm[i]];
+            edges_perm[i] = p[edges_perm[i]];
         }
     }
 }
--- src/cube/edges.h
+++ src/cube/edges.h
@@ -1,9 +1,15 @@
 #include <vector>
+#include <array>
+
+using Table12 = std::array<int, 12>;
 
 class Edges {
 public:
-    Edges();
+    Edges(int cnt = 12);
     std::vector<int> edges_perm;
     std::vector<int> edges_orient;
     void apply_move(int move);
+private:
+    int cnt;
+    std::array<Table12, 6> perm, orient;
 };
