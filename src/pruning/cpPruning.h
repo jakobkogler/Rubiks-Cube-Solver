9600d12869fef6f64c01a746c566985acea39f5e
Jakob Kogler <jakob.kogler@gmail.com>
2018-06-19 20:27:55 +0200

    Remove co/cp/eo/ep/ep2

--- src/pruning/cpPruning.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "prune.h"
-
-class cpPruning : public Prune {
-public:
-    cpPruning();
-    int pruning_number(Cube &cube);
-};
--- src/pruning/ep1Pruning.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "prune.h"
-
-class ep1Pruning : public Prune {
-public:
-    ep1Pruning();
-    int pruning_number(Cube &cube);
-};
